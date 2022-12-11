494cbf6648c91f4ec88994dda549fbb4a9568e27
Auf <mrabdurrahmanauf@gmail.com>
2022-12-11 17:29:44 +0700

    Merapikan tampilan detail gambar

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,21 +1,23 @@
 const createDetailTemplate = (data) => `
   <div class="job_card ">
-    <img class="job_img" src="${data.pictureId}"  alt="${data.title}" />
+    <img class="job_img" src="${data.company_logo_url}"  alt="${data.title}" />
     <div class="job_info">
       <h3>Information</h3>
-      <h4>Tagline</h4>
-      <p class="job__name">${data.company}</p>
+      <h4>Vacancy</h4>
+      <p class="job__name">${data.title}</p>
       <h4>Location</h4>
       <p>${data.location}</p>
       <h4>Salary</h4>
       <p>${data.salary}</p>
     </div>
     <div class="job_description">
-      <h3>${data.title}</h3>
+      <h3>${data.company}</h3>
       <p>${data.description}</p>
+      <h3>Job Qualification</h3>
       <p>${data.requirements}</p>
+      <h3>Job Description</h3>
       <p>${data.job_desks}</p>
-      <a href="${data.link}" target="_blank" class="btn apply-btn text-white">Kirim Lamaran</a>
+      <a href="${data.link}" target="_blank" class="btn apply-btn text-white">Kirim Lamaran »</a>
     </div>
   </div>
 `;
@@ -23,7 +25,7 @@ const createDetailTemplate = (data) => `
 const createItemTemplate = (data) => `
     <div class="col-12 col-md-6 col-lg-4 my-3 justify-content-center">
         <div class="card shadow" style="width: 18rem;">
-        <img src="../image/loker.jpg" class="card-img-top" alt="...">
+        <img src="${data.company_logo_url}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h3 class="card-title"><a href="/#/detail/${data.id}" style="text-decoration: none" class="card-title"> ${data.title}</a></h3>
                 <h4 class="office-loc">${data.location}</h4>
@@ -32,7 +34,7 @@ const createItemTemplate = (data) => `
                 <!-- <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem hic ad 
                 neque omnis adipisci eveniet minima dolores explicabo recusandae dicta est, officiis 
                 similique magni aspernatur aliquid fugiat accusantium, sint ratione.</p> -->
-                <a href="${data.link}" target="_blank" class="btn apply-btn text-white">Kirim Lamaran</a>
+                <a href="${data.link}" target="_blank" class="btn apply-btn text-white">Kirim Lamaran »</a>
                 <div class="saved-job"></div>
             </div>
         </div>
@@ -50,41 +52,41 @@ const createAboutTemplate = () => `
   <div class="row text-center">
   <div class="col-lg-5 mx-auto distance-1">
     <div class="card shadow mx-auto" style="max-width: 23rem;">
-      <img class="rounded-circle mx-auto" src="../image/Zulfadar_profile.jpg" alt="Generic placeholder image" width="140" height="140">
+      <img class="rounded-circle mx-auto" src="../image/Zulfadar_profile.jpg" alt="Generic placeholder image" width="200" height="200">
       <div class="card-body">
         <h2>Zulfadar Indaka Alkaf</h2>
         <p>Front End & UI Designer</p>
-        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/zulfadar-indaka-alkaf-097b82255/" target="_blank" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/zulfadar-indaka-alkaf-097b82255/" target="_blank" role="button">Connect »</a></p>
       </div>
     </div>
   </div>
   <div class="col-lg-5 mx-auto distance-1">
     <div class="card shadow mx-auto" style="max-width: 23rem;">
-      <img class="rounded-circle mx-auto" src="../image/auf_profile.png" alt="Generic placeholder image" width="140" height="140">
+      <img class="rounded-circle mx-auto" src="../image/auf_profile.png" alt="Generic placeholder image" width="200" height="200">
       <div class="card-body">
         <h3>M.R. Abdurrahman’auf A</h3>
         <p>Front End</p>
-        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/abdurrahman-auf-569872244/" target="_blank" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/abdurrahman-auf-569872244/" target="_blank" role="button"> Connect »</a></p>
       </div>
     </div>
   </div>
   <div class="col-lg-5 mx-auto distance-1">
     <div class="card shadow mx-auto" style="max-width: 23rem;">
-      <img class="rounded-circle mx-auto" src="../image/galuh_profile.jpg" alt="Generic placeholder image" width="140" height="140">
+      <img class="rounded-circle mx-auto" src="../image/galuh_profile.jpg" alt="Generic placeholder image" width="200" height="200">
       <div class="card-body">
         <h2>Galuh Widy Nugroho</h2>
         <p>Front End</p>
-        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/galuhwidynugroho/" target="_blank" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/galuhwidynugroho/" target="_blank" role="button">Connect »</a></p>
       </div>
     </div>
   </div>
   <div class="col-lg-5 mx-auto distance-1">
     <div class="card shadow mx-auto" style="max-width: 23rem;">
-      <img class="rounded-circle mx-auto" src="../image/faiz_profile.jpeg" alt="Generic placeholder image" width="140" height="140">
+      <img class="rounded-circle mx-auto" src="../image/faiz_profile.jpeg" alt="Generic placeholder image" width="200" height="200">
       <div class="card-body">
         <h2>Faiz Gunawan</h2>
         <p>Front End</p>
-        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/faiz-gunawan/" target="_blank" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary " href="https://www.linkedin.com/in/faiz-gunawan/" target="_blank" role="button">Connect »</a></p>
       </div>
     </div>
   </div>
